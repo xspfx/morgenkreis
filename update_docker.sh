3667d4270a6bb61b62a5eb0775a287cbbd9c1b65
Stefan Pfeiffer <stefan.pfeiffer@posteo.de>
2025-09-19 10:15:21 +0200

    update docker image account

--- update_docker.sh
+++ update_docker.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 # build the productive container
-docker build --target dev_image -t 3tr4xx/python_playground:amd64 -f ./docker/Dockerfile .
-docker push 3tr4xx/python_playground:amd64
+docker build --target dev_image -t xspfx/morgenkreis_dev:latest -f ./docker/Dockerfile .
+docker push xspfx/morgenkreis_dev:latest
